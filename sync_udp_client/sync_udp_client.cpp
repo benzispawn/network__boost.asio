0829e6820066f41a21f6b960c705021ea85851cd
benzispawn <spawnbenzi@gmail.com>
2020-11-11 00:04:01 -0300

    unfinished async tcp client

---
+++ sync_udp_client/sync_udp_client.cpp
@@ -0,0 +1,41 @@
+#include "sync_udp_client.h"
+
+int main()
+{
+    const std::string server1_raw_ip_address = "127.0.0.1";
+    const unsigned short server1_port_num = 3333;
+
+    const std::string server2_raw_ip_address = "192.168.122.188";
+    const unsigned short server2_port_num = 3334;
+
+    try
+    {
+        SyncUDPClient client;
+        std::cout << "Sending request to the server #1 ..."
+            << std::endl;
+
+        std::string response = 
+            client.emulateLongComputationOp(10, 
+                server1_raw_ip_address, 
+                server1_port_num);
+
+        std::cout << "Sending request to the server #2 ..."
+            << std::endl;
+
+        response = 
+            client.emulateLongComputationOp(10,
+                server2_raw_ip_address,
+                server2_port_num);
+
+        std::cout << "Response from the server #2 received: "
+            << response << std::endl;
+    }   
+    catch(boost::system::system_error &e)
+    {
+        std::cout << "Error occured! Error code = "
+            << e.code() << ". Message: " << e.what();
+        
+        return e.code().value();
+    }
+    return 0;    
+}
---
+++ sync_udp_client/sync_udp_client.h
@@ -0,0 +1,42 @@
+#include <boost/asio.hpp>
+#include <iostream>
+
+class SyncUDPClient {
+    public:
+        SyncUDPClient() :
+            m_sock(m_ios) {
+                m_sock.open(boost::asio::ip::udp::v4());
+            }
+        std::string emulateLongComputationOp(
+            unsigned int duration_sec,
+            const std::string& raw_ip_address,
+            unsigned short port_num) {
+                std::string request = "EMULATE_LONG_COMP_OP "
+                    + std::to_string(duration_sec)
+                    + "\n";
+
+                boost::asio::ip::udp::endpoint ep(
+                    boost::asio::ip::address::from_string(raw_ip_address),
+                    port_num);
+
+                sendRequest(ep, request);
+                return receiveResponse(ep);
+            };
+    private:
+        boost::asio::io_service m_ios;
+        boost::asio::ip::udp::socket m_sock;
+
+        void sendRequest(const boost::asio::ip::udp::endpoint& ep,
+                const std::string& request) {
+                    m_sock.send_to(boost::asio::buffer(request), ep);
+                }
+
+                std::string receiveResponse(boost::asio::ip::udp::endpoint& ep) {
+                    char response[6];
+                    std::size_t bytes_recieved =
+                        m_sock.receive_from(boost::asio::buffer(response), ep);
+
+                    m_sock.shutdown(boost::asio::ip::udp::socket::shutdown_both);
+                    return std::string(response, bytes_recieved);
+                }
+};
